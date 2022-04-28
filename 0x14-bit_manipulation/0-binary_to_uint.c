fbcc6d4cdf21eea50de3ddb61135a07cbeff7abb
tundelakins <tundelakins4real@yahoo.com>
2022-04-28 20:55:21 +0000

    This is a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,48 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * binary_to_uint - convert binary to decimal.
- * @b: the binary number in string format
- * Description: convert binary to decimal
- * section header: the header of this function is header.h
- * Return: this return the convert number.
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+*/
+
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
+
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number
+ *
+ * Return: 0 or converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result, base;
-	int i;
+	unsigned int n = 0;
+	int i, len;
 
-	result = 0;
-	i = 0;
-	base = 1;
-
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	while (*(b + i))
+	len = _strlen(b);
+
+	for (i = 0; i != len; i++)
 	{
-		if (*(b + i) != '0' && *(b + i) != '1')
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
 			return (0);
-		i++;
-	}
-	for (i--; i >= 0; i--)
-	{
-		if (*(b + i) == '1')
-			result = result + base;
-		base = base * 2;
 	}
 
-	return (result);
+	return (n);
 }
