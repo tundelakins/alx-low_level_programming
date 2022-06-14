e48d40cf0c0f9a9803cf5a2f09b7ce34f49ef734
tundelakins <tundelakins4real@yahoo.com>
2022-06-14 08:11:04 +0000

    Fixes

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integer
+ * @a: an integer to be added
+ * @b: second integer to be added
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integer
+ * @a: an integer to be subtracted
+ * @b: second integer to be subtracted
+ * Return: result of sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integer
+ * @a: an integer to be multiplied
+ * @b: second integer to be multiplied
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integer
+ * @a: an integer
+ * @b: second integer
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulo of two integers
+ * @a: an integer
+ * @b: second integer
+ * Return: result of modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
